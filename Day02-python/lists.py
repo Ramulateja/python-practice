68be317e5ef775a018f5f3243fc102869f69a381
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-17 00:38:45 +0530

    day2files

--- Day02-python/lists.py
+++ Day02-python/lists.py
@@ -4,3 +4,9 @@ items.append("hello")#it is used to add the add the items at the end of the list
 items[1]="ball" #updates the existing index value with new one 
 items.pop(0)#removes the items from the list based on the given index if not mentioned removes the last
 print(items)
+list1=[5,4,3,2,1]
+list2=[9,8,7,6]
+list1.sort()#sorts them into ascending order
+list2.sort()
+list1.extend(list2)#combines the both values into the single list
+print(list1)
