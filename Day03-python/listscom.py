e58f955cbd46270d5b3ec2f4db9e301468c1ab7e
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-17 23:42:55 +0530

    day3

--- Day03-python/listscom.py
+++ Day03-python/listscom.py
@@ -1,7 +1,9 @@
 # it is a datastructure used in python totally ds contains 4types other 3 are tuples,sets and dictionaries.
-lists=[]
+lists=[]# lists are ordered and allow duplicates and changeable as well
 for i in range(1,6):
     lists.append(i)
+
+#list compensation
 print(lists)
 list1=[i for i in range(1,6)]#it add the values from 1 to 5 in list with using the loop inside of the list using the list comperhension
 print(list1)
