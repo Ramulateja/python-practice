6f535f856b62236ba4d30351e176f69979bbbe8d
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-17 15:13:49 +0530

    day3

---
+++ Day03-python/listscom.py
@@ -0,0 +1,6 @@
+lists=[]
+for i in range(1,6):
+    lists.append(i)
+print(lists)
+list1=[i for i in range(1,6)]#it add the values from 1 to 5 in list with using the loop inside of the list using the list comperhension
+print(list1)
