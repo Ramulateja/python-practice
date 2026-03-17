b2c5eaf4614a75d7faff708768c318cf1bd0e21f
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-18 02:29:09 +0530

    day4func

---
+++ Day04-python/module.py
@@ -0,0 +1,5 @@
+#modulo is like a librarie that we can create and use
+def min(a,b):
+    print(a-b)
+min(2,3)
+#we created the file name as module.py then i written a function min now i create a new file and import this file name module
---
+++ Day04-python/moduleusage.py
@@ -0,0 +1,2 @@
+import module #i was imported the file and i used it
+min(3,5)
