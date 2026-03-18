d0f48f67793bd4acca07b7bf45c6418db8df4c39
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-19 00:24:00 +0530

    day5prac

--- Day05-python/prac.py
+++ Day05-python/prac.py
@@ -1,3 +1,6 @@
 def reverseString(s):
     print(s[::-1])
 reverseString("Hello")# reversing the string problem from the geeks for geeks
+
+for i in range(5, -1,-1):#(start,end,step)
+    print(i, end=",")
