938e656190baaf384cac620e18aba631f86b3099
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-19 17:46:58 +0530

    day2arr

--- Day02-python/arrays.py
+++ Day02-python/arrays.py
@@ -4,5 +4,5 @@ print(arr[0:2])#or arr[:2]# it's called slicing it prints the elements from 0-1
 #upper and lower case modification
 up="hello"
 print(up.upper())
-low="HELLO"
+low="HELLo"
 print(low.lower())
