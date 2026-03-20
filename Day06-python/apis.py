f75845f1b7f871ea1eb9089fdbe61b36bc0b60d8
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-21 00:39:27 +0530

    api change

--- Day06-python/apis.py
+++ Day06-python/apis.py
@@ -1,5 +1,6 @@
 import requests
+# now the res store the respond of the request we send to server ex:"joke server"then server gives respond the value in the form of json.
 res = requests.get("https://official-joke-api.appspot.com/random_joke")
-data = res.json()
+data = res.json()#the .json converts yhe res into json 
 print(data["setup"])
 print(data["punchline"])
