d45fad56fd8cdf66d845e135694fc067cc7348be
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-21 23:42:27 +0530

    init-selfcomm

--- Day07-python/selfparameter.py
+++ Day07-python/selfparameter.py
@@ -1,7 +1,7 @@
 class person1:
-    def __init__(self,name):
+    def __init__(self,name):#self is the parameter it represent current object which is currently called.(like p1.greet())
         self.name=name
     def greet(self):
         print("hello"+self.name)
 p1=person1("tej")
-p1.greet()
+p1.greet()# self become the p1 in place of self.
