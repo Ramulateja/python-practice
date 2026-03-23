5b4dd3101750c6f5cffc3ff78b9093a3c8a00df2
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-23 21:43:19 +0530

    code

--- Day08-python/encapsulation.py
+++ Day08-python/encapsulation.py
@@ -1,3 +1,8 @@
 #Encapsulation is used to protect data from the direct access to a data.
 #It is used to protect the data from unauthorized access and modification.
-#Encapsulation is hiding data and 
+#Encapsulation is hiding data and controlling access with methods.
+class stu:
+    def __init__(self,salary):
+        self.salary=10000
+s=stu()
+print(s.salary)
