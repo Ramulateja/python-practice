3bb0a6a6f11267a3572573c3de846c276f56ae09
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-03-23 21:39:32 +0530

    comments

--- Day08-python/encapsulation.py
+++ Day08-python/encapsulation.py
@@ -1,1 +1,3 @@
 #Encapsulation is used to protect data from the direct access to a data.
+#It is used to protect the data from unauthorized access and modification.
+#Encapsulation is hiding data and 
